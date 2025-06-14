e34e3127b2e8a3a6e6d9c406af65ce49407d5645
Bhautik Bavadiya <63995315+yesbhautik@users.noreply.github.com>
2025-06-14 17:55:11 +0530

    Revert "Added payment gateway!"

--- app/pages/pay.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-// Redirect /pay to /payment to maintain compatibility
-await navigateTo('/payment', { replace: true })
-</script>
-
-<template>
-  <div>Redirecting to payment...</div>
-</template> 
